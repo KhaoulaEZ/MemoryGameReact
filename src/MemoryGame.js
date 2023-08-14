056eff8c597d6b4806b8fbcb4df891a466e74404
KhaoulaEZ <ezaoui.khaoula@gmail.com>
2023-08-14 11:54:58 +0100

    First commit to memeory game

---
+++ src/MemoryGame.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+
+const GRID_SIZE = 6;
+const TOTAL_TILES = GRID_SIZE * GRID_SIZE;
+
+const generateRandomNumbers = () => {
+  const numbers = Array.from({ length: TOTAL_TILES / 2 }, (_, index) => index + 1);
+  return [...numbers, ...numbers].sort(() => Math.random() - 0.5);
+};
+
+const MemoryGame = () => {
+  const [tiles, setTiles] = useState([]);
+  const [selectedTiles, setSelectedTiles] = useState([]);
+  
+  useEffect(() => {
+    const randomNumbers = generateRandomNumbers();
+    const initialTiles = randomNumbers.map((number, index) => ({
+      id: index,
+      number,
+      flipped: false,
+    }));
+    setTiles(initialTiles);
+  }, []);
+  
+  const handleTileClick = (id) => {
+    if (selectedTiles.length < 2) {
+      const tileIndex = tiles.findIndex(tile => tile.id === id);
+      const updatedTiles = [...tiles];
+      updatedTiles[tileIndex].flipped = true;
+      setTiles(updatedTiles);
+      setSelectedTiles([...selectedTiles, tiles[tileIndex]]);
+    }
+  };
+
+  useEffect(() => {
+    if (selectedTiles.length === 2) {
+      if (selectedTiles[0].number === selectedTiles[1].number) {
+        setSelectedTiles([]);
+      } else {
+        setTimeout(() => {
+          const updatedTiles = tiles.map(tile => ({
+            ...tile,
+            flipped: false,
+          }));
+          setTiles(updatedTiles);
+          setSelectedTiles([]);
+        }, 1000);
+      }
+    }
+  }, [selectedTiles, tiles]);
+
+  return (
+    <div className="memory-game">
+      <div className="grid">
+        {tiles.map(tile => (
+          <div
+            key={tile.id}
+            className={`tile ${tile.flipped ? 'flipped' : ''}`}
+            onClick={() => !tile.flipped && handleTileClick(tile.id)}
+          >
+            {tile.flipped ? tile.number : ''}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MemoryGame;
